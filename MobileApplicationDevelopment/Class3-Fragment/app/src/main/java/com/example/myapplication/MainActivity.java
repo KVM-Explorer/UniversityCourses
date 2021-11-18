468ed7004e79917a317f3815c2403954cb6015cc
Lightning <wfcyywh1225@126.com>
2021-11-18 23:45:54 +0800

    上传第三次移动应用开发作业

---
+++ MobileApplicationDevelopment/Class3-Fragment/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
