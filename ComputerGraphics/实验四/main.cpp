6c16c06ebf6b71343f58d45e79375bae4d78f7b6
Lightning <wfcyywh1225@126.com>
2021-11-20 23:02:42 +0800

    光照、视角基本正常

--- ComputerGraphics/实验四/main.cpp
+++ ComputerGraphics/实验四/main.cpp
@@ -47,21 +47,19 @@ void RotatedLookAt(float theta)
     Header[2] = -cos(angle);
 }
 
-
 GLuint LoadTexture(char* name)
 {
     GLuint tID;
 
-    int width ,height,channels;
+    int width ,height;
     auto image = cv::imread(name);
     width = image.cols;
     height = image.rows;
-    channels = image.channels();
 
     glGenTextures(1,&tID);
     glBindTexture(GL_TEXTURE_2D,tID);
 
-    glTexEnvf(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_DECAL);
+
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
@@ -118,89 +116,29 @@ void display()
     glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
     //地面
     glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, textureWall);
-    glBegin(GL_QUADS);
-    glColor3f(1.0f,1.0f,1.0f);
+    glBindTexture(GL_TEXTURE_2D, textureID1);
+    glBegin(GL_POLYGON);
+//    glColor3f(1.0f,1.0f,1.0f);
 
     glTexCoord2f(0.0f,0.0f);//指定顶点纹理坐标
     glNormal3f(0,1,0);//指定顶点法向量
-    glVertex3f(-50.0f,0.0f,50.0f);//指定顶点坐标
+    glVertex3f(-50.0f,-1.0f,50.0f);//指定顶点坐标
 
     glTexCoord2f(1.0f,0.0f);
     glNormal3f(0,1,0);
-    glVertex3f(50.0f,0.0f,50.0f);
+    glVertex3f(50.0f,-1.0f,50.0f);
 
     glTexCoord2f(1.0f,1.0f);
     glNormal3f(0,1,0);
-    glVertex3f(50.0f,0.0f,-50.0f);
+    glVertex3f(50.0f,-1.0f,-50.0f);
 
     glTexCoord2f(0.0f,1.0f);
     glNormal3f(0,1,0);
-    glVertex3f(-50.0f,0.0f,-50.0f);
+    glVertex3f(-50.0f,-1.0f,-50.0f);
     glEnd();
     glDisable(GL_TEXTURE_2D);
 
-//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//    //光照
-//    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-//    glLightfv(GL_LIGHT0, GL_AMBIENT, light_color);
-//    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_color);
-//    glLightfv(GL_LIGHT0, GL_SPECULAR, light_color);
-//
-//    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, aMaterial);
-//    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, dMaterial);
-//    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, sMaterial);
-//    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, &shiniess);
-//
-//    glEnable(GL_LIGHTING);
-//    glEnable(GL_LIGHT0);
-//    for(int i=0;i<3;i++) std::cout<<light_position[i]<<' ';
-//    std::cout<<endl;
-//
-//    GLenum err;
-//    if ((err=glGetError())!=GL_NO_ERROR)
-//    {
-//        std::cout<<err<<std::endl;
-//        exit(0);
-//    }
-//
-//    // 观察矩阵初始化
-//    glMatrixMode(GL_MODELVIEW);
-//    glLoadIdentity();
-//
-//    float at[3];
-//    for (int i=0;i<3;i++)
-//    {
-//        at[i] = Position[i]+Header[i];
-//    }
-//
-//    gluLookAt(Position[0],Position[1],Position[2],
-//              at[0],at[1],at[2],
-//              0,1,0);
-//    std::cout<<Header[0]<<' '<<Header[1]<<' '<<Header[2]<<std::endl;
-//
-//    // 地面
-//    glEnable(GL_TEXTURE_2D);
-//    glBindTexture(GL_TEXTURE_2D, textureGround);
-//    glColor3f(1,1,1);
-//    glBegin(GL_POLYGON);
-//    glTexCoord2f(0,0);
-//    glNormal3f(0,1,0);
-//    glVertex3f(-50,-20,-50);
-//
-//    glTexCoord2f(0,10);
-//    glNormal3f(0,1,0);
-//    glVertex3f(50,-20,-50);
-//
-//    glTexCoord2f(10,10);
-//    glNormal3f(0,1,0);
-//    glVertex3f(50,-20,50);
-//
-//    glTexCoord2f(10,0);
-//    glNormal3f(0,1,0);
-//    glVertex3f(-50,-20,50);
-//    glEnd();
-//    glDisable(GL_TEXTURE_2D);
+
 
 //   天空
 //    glEnable(GL_TEXTURE_2D);
@@ -208,22 +146,22 @@ void display()
 //    glBegin(GL_QUADS);
 //    glTexCoord2f(0,0);
 //    glNormal3f(0,1,0);
-//    glVertex3f(-500,500,-200);
+//    glVertex3f(-50,50,-20);
 //    glTexCoord2f(0,10);
 //    glNormal3f(0,1,0);
-//    glVertex3f(500,500,-200);
+//    glVertex3f(50,50,-20);
 //    glTexCoord2f(10,10);
 //    glNormal3f(0,1,0);
-//    glVertex3f(500,-200,-200);
+//    glVertex3f(50,-20,-20);
 //    glTexCoord2f(10,0);
 //    glNormal3f(0,1,0);
-//    glVertex3f(-500,-200,-200);
+//    glVertex3f(-50,-20,-20);
 //    glEnd();
 //    glDisable(GL_TEXTURE_2D);
 //
 //    //  绘制球、正方体、圆柱体
-//    Sphere.Render(textureSphere);
-//    Cube.Render(textureWall);
+    Sphere.Render(textureSphere);
+    Cube.Render(textureWall);
 ////    Cylinder.Render(textureFun);
 
     glutSwapBuffers();
@@ -328,19 +266,19 @@ void init()
 {
     glClearColor(0.5, 1.0, 1.0, 1.0);
     glEnable(GL_DEPTH_TEST);
-//    textureID1 = LoadTexture("timg.jpg");
-//    textureGround = LoadTexture("timg.jpg");
-//    textureSphere = LoadTexture("Emjo.png");
-//    textureSky = LoadTexture("sky.png");
+    textureID1 = LoadTexture("timg.jpg");
+    textureGround = LoadTexture("timg.jpg");
+    textureSphere = LoadTexture("Emjo.png");
+    textureSky = LoadTexture("sky.png");
     textureWall = LoadTexture("wall.png");
-//    textureFun = LoadTexture("FUN.png");
-//    Sphere.InitData(30);
-//    Sphere.SetPos(cgPoint3D(10.f,20.f,30.f));
-//    Cube.InitData(30);
-//    Cube.SetPos(cgPoint3D(50,50,30));
-//    Cylinder.InitData(10,20);
-//    Cylinder.SetPos(cgPoint3D(-10,-10,-10));
-//    LightBox.InitData(5);
+    textureFun = LoadTexture("FUN.png");
+    Sphere.InitData(30);
+    Sphere.SetPos(cgPoint3D(10.f,20.f,30.f));
+    Cube.InitData(30);
+    Cube.SetPos(cgPoint3D(50,50,30));
+    Cylinder.InitData(10,20);
+    Cylinder.SetPos(cgPoint3D(-10,-10,-10));
+    LightBox.InitData(5);
 }
 int main(int argc,char** argv)
 {
